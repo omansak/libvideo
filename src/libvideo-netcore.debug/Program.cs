4dc59ec1b8a1a8b1337a59539e4bbbd87ca380f1
Adrien Torris <adrientorris@gmail.com>
2016-11-16 07:05:39 +0100

    add .NET Core debug console app

--- src/libvideo-netcore.debug/Program.cs
+++ src/libvideo-netcore.debug/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using VideoLibraryNetCore;
@@ -11,73 +12,29 @@ namespace libvideo_netcore.debug
     {
         public static void Main(string[] args)
         {
+            WriteVideos();
+        }
+
+        static void WriteVideos()
+        {
+            string localPath = @"C:\Users\atorris\Documents\Nouveau dossier (2)\videos\";
+
             string[] queries =
                {
                 "https://www.youtube.com/watch?v=eQfJA24m7IIA",
                 "https://www.youtube.com/watch?v=2vH5Yoaia20"
             };
 
-            TestYoutubeExtractor(queries);
-            TestVideoLib(queries);
-            Console.WriteLine("Done.");
+            var youTube = YouTube.Default; // starting point for YouTube actions
+            foreach (string s in queries)
+            {
+                Console.WriteLine("START " + s);
+                var video = youTube.GetVideo(s); // gets a Video object with info about the video
+                File.WriteAllBytes(localPath + video.FullName, video.GetBytes());
+                Console.WriteLine("DONE " + s);
+            }
+
             Console.ReadKey();
-        }
-
-        public static void TestVideoLib(string[] queries)
-        {
-            using (var cli = Client.For(YouTube.Default))
-            {
-                Console.WriteLine("Downloading...");
-                for (int i = 0; i < queries.Length; i++)
-                {
-                    string uri = queries[i];
-
-                    try
-                    {
-                        var videoInfos = cli.GetAllVideosAsync(uri).GetAwaiter().GetResult();
-
-                        Console.WriteLine($"Link #{i + 1}");
-                        foreach (var v in videoInfos)
-                        {
-                            Console.WriteLine(v.Uri);
-                            Console.WriteLine();
-                        }
-                    }
-                    catch
-                    {
-                        Debugger.Break();
-                    }
-                }
-            }
-        }
-
-        public static void TestYoutubeExtractor(string[] queries)
-        {
-            using (var cli = Client.For(YouTube.Default))
-            {
-                for (int i = 0; i < queries.Length; i++)
-                {
-                    string query = queries[i];
-
-                    var video = cli.GetVideo(query);
-                    string uri = video.Uri;
-
-                    try
-                    {
-                        var uris = DownloadUrlResolver
-                            .GetDownloadUrls(query)
-                            .Select(v => v.DownloadUrl);
-
-                        Console.WriteLine($"Link #{i + 1}");
-                        foreach (var v in uris)
-                        {
-                            Console.WriteLine(v);
-                            Console.WriteLine();
-                        }
-                    }
-                    catch { }
-                }
-            }
         }
     }
 }
