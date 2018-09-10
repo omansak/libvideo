742f3124c73bb7a16cbbac77340b39da84b0267d
OMANSAK <omansakosk@gmail.com>
2018-09-10 09:15:06 +0300

    Update YouTubeVideo.Decrypt.cs

--- src/libvideo/YouTubeVideo.Decrypt.cs
+++ src/libvideo/YouTubeVideo.Decrypt.cs
@@ -14,7 +14,7 @@ namespace VideoLibrary
         //Static 
         //private static readonly Regex DecryptionFunctionRegex_1 = new Regex(@"(?!h\.)(.*?)=function\(\w\){.*\.split\(""""\);.*\.join\(""""\)}");
         //or Static @hig-dev
-        //private static readonly Regex DecryptionFunctionRegex_2 = Regex.Match(js, @"(\w+)&&(\w+)\.set\(\w+,(\w+)\(\1\)\);return\s+\2").Groups[3].Value;
+        //private static readonly Regex DecryptionFunctionRegex_2 = Regex(@"(\w+)&&(\w+)\.set\(\w+,(\w+)\(\1\)\);return\s+\2");
         //Dynamic with Service
         private static Regex DFunctionRegex;
         private static readonly Regex FunctionRegex = new Regex(@"\w+\.(\w+)\(");
