7d957a2a5f3741f05dc25cf552051dcc907ee7cc
Halil <hig@outlook.com>
2018-11-07 17:20:23 +0100

    Fix YouTube decryption again

--- src/libvideo/YouTubeVideo.Decrypt.cs
+++ src/libvideo/YouTubeVideo.Decrypt.cs
@@ -37,7 +37,7 @@ namespace VideoLibrary
         private string DecryptedSignature(string signature, string js)
         {
             // Find the name of the function that handles deciphering
-            var entryPoint = Regex.Match(js, @"(\w+)&&(\w+)\.set\(\w+,(\w+)\(\1\)\);return\s+\2").Groups[3].Value;
+            var entryPoint = Regex.Match(js, @"\bc\s*&&\s*d\.set\([^,]+\s*,\s*\([^)]*\)\s*\(\s*([a-zA-Z0-9$]+)\(").Groups[1].Value;
             if (String.IsNullOrWhiteSpace(entryPoint))
                 throw new Exception("Could not find the entry function for signature deciphering.");
 
