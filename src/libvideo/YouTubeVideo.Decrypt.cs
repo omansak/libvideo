d3aef5d0188bdba1194098e78e377f4ae2757046
Osman Şakir Kapar <omansakosk@outlook.com>
2019-01-19 01:32:32 +0300

    Fix whitespaces

--- src/libvideo/YouTubeVideo.Decrypt.cs
+++ src/libvideo/YouTubeVideo.Decrypt.cs
@@ -11,7 +11,7 @@ namespace VideoLibrary
 {
     public partial class YouTubeVideo
     {
-        //Static @omansak
+        //Static
         private static readonly Regex DecryptionFunctionRegex_Static_1 = new Regex(@"\bc\s*&&\s*d\.set\([^,]+\s*,[^(]*\(([a-zA-Z0-9$]+)\(");       
         //Dynamic with Service
         private static Regex DFunctionRegex_Dynamic;
